f51e9560937bc0dc6c1554292d19dfdec0779041
daikideal <54879050+daikideal@users.noreply.github.com>
2019-09-11 08:07:13 +0000

    Edit root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -10,5 +12,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, 
   # see http://guides.rubyonrails.org/routing.html
   
-  root 'application#hello'
 end
