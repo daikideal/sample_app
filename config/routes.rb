1e4b2c830517703919fdfcf24b77ed15dbaf23b6
daikideal <54879050+daikideal@users.noreply.github.com>
2019-09-22 14:48:03 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#home'
-
+  
   get '/help', to:'static_pages#help'
 
   get '/about', to:'static_pages#about'
@@ -8,6 +9,12 @@ Rails.application.routes.draw do
   get '/contact', to:'static_pages#contact'
   
   get '/signup', to:'users#new'
+  
+  get '/login', to:'sessions#new'
+  
+  post '/login', to:'sessions#create'
+  
+  delete '/logout', to:'sessions#destroy'
   
   post '/signup', to:'users#create'
   
