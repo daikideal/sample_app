dd9f1383596c999e67d4c3b946dc2db090d590ed
daikideal <54879050+daikideal@users.noreply.github.com>
2019-09-26 17:30:02 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   
   def show
     @user = User.find(params[:id])
@@ -6,6 +9,10 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   def create
@@ -19,11 +26,53 @@ class UsersController < ApplicationController
     end
   end
   
-  private
+  def edit
+  end
   
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  private
+    
+    # 管理者以外のユーザーが編集できるのは以下の属性のみである
     def user_params
       params.require(:user).permit(:name, :email, :password, 
                                     :password_confirmation)
     end
-  
+    
+    # beforeアクション
+    
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    # 正しいユーザーかどうか確認(editとupdateから@userへの代入を削除))
+    def correct_user
+      @user = User.find(params[:id])
+      # 正しいユーザー以外はルートページへリダイレクト
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+    
 end
